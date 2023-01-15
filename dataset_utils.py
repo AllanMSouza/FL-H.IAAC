1b24736582dcf20d9b0b114ce5b9ced6c1a3a0be
Wellington Viana Lobato Junior <wellington.lobato.junior@gmail.com>
2023-01-15 14:14:25 -0300

    Update dataset_utils.py
    Just changed the dataset mnist to cifar10.

--- dataset_utils.py
+++ dataset_utils.py
@@ -110,7 +110,7 @@ class ManageDatasets():
 
 		else:
 
-			(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+			(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 			x_train, x_test                      = x_train/255.0, x_test/255.0
 			x_train, y_train, x_test, y_test     = self.slipt_dataset(x_train, y_train, x_test, y_test, n_clients)
 
