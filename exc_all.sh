6c71d391f224c96ad3a50419a196443bfb45a4f2
Cláudio <claudiogs.capanema@gmail.com>
2023-02-05 23:25:02 -0300

    Bugfix

--- exc_all.sh
+++ exc_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-git checkout FedPer2
-git fetch
-git pull
+#git checkout FedPer2
+#git fetch
+#git pull
 
 #echo "Executing experiment 1..."
 #python3 exec_simulation.py --experiment_id=1 > execution_log/experiment_1.txt 2>&1
 
-echo "Executing experiment 2..."
-python3 exec_simulation.py --experiment_id=2 > execution_log/experiment_2.txt 2>&1
+#echo "Executing experiment 2..."
+#python3 exec_simulation.py --experiment_id=2 > execution_log/experiment_2.txt 2>&1
 #
 #echo "Executing experiment 3..."
 #python3 exec_simulation.py --experiment_id=3 > execution_log/experiment_3.txt 2>&1
@@ -18,7 +18,7 @@ python3 exec_simulation.py --experiment_id=2 > execution_log/experiment_2.txt 2>
 #echo "Executing experiment 5..."
 #python3 exec_simulation.py --experiment_id=5 > execution_log/experiment_5.txt 2>&1
 hostname=$(hostname)
-if [[ hostname != "claudio-Predator-PH315-52" ]]
+if [[ $hostname == "claudio-Predator-PH315-52" ]]
 then
   echo ""
 else
