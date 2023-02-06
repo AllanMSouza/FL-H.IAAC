cc02fc1b2e3c1a628d142f4ae08cb6e6d0f14d97
Cláudio <claudiogs.capanema@gmail.com>
2023-02-05 23:35:42 -0300

    Bugfix

--- exc_all.sh
+++ exc_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#git checkout FedPer2
-#git fetch
-#git pull
+git checkout FedPer2
+git fetch
+git pull
 
 #echo "Executing experiment 1..."
 #python3 exec_simulation.py --experiment_id=1 > execution_log/experiment_1.txt 2>&1
 
-#echo "Executing experiment 2..."
-#python3 exec_simulation.py --experiment_id=2 > execution_log/experiment_2.txt 2>&1
+echo "Executing experiment 2..."
+python3 exec_simulation.py --experiment_id=2 > execution_log/experiment_2.txt 2>&1
 #
 #echo "Executing experiment 3..."
 #python3 exec_simulation.py --experiment_id=3 > execution_log/experiment_3.txt 2>&1
