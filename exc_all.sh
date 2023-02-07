91b3f5d6bb5b960350f9e35828dc18db40d64fdc
Cláudio <claudiogs.capanema@gmail.com>
2023-02-06 21:20:17 -0300

    Bugfix

--- exc_all.sh
+++ exc_all.sh
@@ -9,8 +9,8 @@ git pull
 echo "Executing experiment 2..."
 python3 exec_simulation.py --experiment_id=2 > execution_log/experiment_2.txt 2>&1
 
-#echo "Executing experiment 3..."
-#python3 exec_simulation.py --experiment_id=3 > execution_log/experiment_3.txt 2>&1
+echo "Executing experiment 3..."
+python3 exec_simulation.py --experiment_id=3 > execution_log/experiment_3.txt 2>&1
 #
 #echo "Executing experiment 4..."
 #python3 exec_simulation.py --experiment_id=4 > execution_log/experiment_4.txt 2>&1
--- exec_simulation.py
+++ exec_simulation.py
@@ -42,7 +42,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 # ======================================================================
 # Configurations
 # DATASETS      				= ('MNIST', 'CIFAR10', )
-DATASETS      					= ('MNIST',)
+DATASETS      					= ('CIFAR10',)
 MODELS        					= ('CNN',)
 ALGORITHMS    					= ('None', 'POC',)
 EPOCHS        					= {'1': [1], '2': [1], '3': [1], '4': [1], '5': [2]}
@@ -55,7 +55,7 @@ NEW_CLIENTS_TRAIN 				= {'FALSE': ('FALSE',), 'TRUE': ('FALSE', 'TRUE',)}
 ROUNDS        					= 100
 # STRATEGIES 					= ('FedAVG', 'FedAvgM', 'FedClassAvg''QFedAvg', 'FedPer', 'FedProto', 'FedYogi', 'FedLocal',)
 STRATEGIES_FOR_ANALYSIS 		= ['FedPredict', 'FedAVG', 'FedProto', 'FedClassAvg', 'FedPer']
-STRATEGIES_TO_EXECUTE 			= ['FedPredict']
+STRATEGIES_TO_EXECUTE 			= ['FedPredict', 'FedAVG', 'FedProto', 'FedClassAvg', 'FedPer']
 
 EXPERIMENTS 		= {1: {'algorithm': 'None', 'new_client': 'False', 'new_client_train': 'False', 'comment': ''},
 					   2: {'algorithm': 'POC', 'new_client': 'False', 'new_client_train': 'False', 'comment': ''},
