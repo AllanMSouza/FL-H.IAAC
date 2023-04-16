13aa3485409a453f0332043cd51b6553883b8238
Cláudio <claudiogs.capanema@gmail.com>
2023-04-16 11:38:26 -0300

    Experimentos

--- analysis/joint_analysis.py
+++ analysis/joint_analysis.py
@@ -10,7 +10,7 @@ class JointAnalysis():
     def __int__(self):
         pass
 
-    def build_filenames_and_read(self, type, strategies, pocs, datasets, experiments, clients='50', model='CNN', file_type='evaluate_client.csv'):
+    def build_filenames_and_read(self, type, strategies, fractions_fit, datasets, experiments, clients='50', model='CNN', file_type='evaluate_client.csv'):
 
         df_concat = None
         count = 0
@@ -21,15 +21,15 @@ class JointAnalysis():
 
             for dataset in datasets:
 
-                for poc in pocs:
+                for fraction_fit in fractions_fit:
 
                     for strategy in strategies:
 
-                        filename = """{}/{}/{}-POC-{}/{}/{}/{}/{}/{}/{}""".format(os.path.abspath(os.path.join(os.getcwd(), 
+                        filename = """{}/{}/{}-None-{}/{}/{}/{}/{}/{}/{}""".format(os.path.abspath(os.path.join(os.getcwd(),
                                                                                                                 os.pardir)) + "/FedLTA/logs",
                                                                                                                 type,
                                                                                                                 strategy,
-                                                                                                                poc,
+                                                                                                                fraction_fit,
                                                                                                                 new_clients,
                                                                                                                 clients,
                                                                                                                 model,
@@ -40,7 +40,7 @@ class JointAnalysis():
                         df = pd.read_csv(filename)
                         df['Strategy'] = np.array([strategy] * len(df))
                         df['Experiment'] = np.array([i] * len(df))
-                        df['POC'] = np.array([poc] * len(df))
+                        df['Fraction fit'] = np.array([fraction_fit] * len(df))
                         df['Dataset'] = np.array([dataset] * len(df))
                         df['Strategy'] = np.array(['FedAvg' if i=='FedAVG' else i for i in df['Strategy'].tolist()])
                         if count == 0:
@@ -49,20 +49,20 @@ class JointAnalysis():
                             df_concat = pd.concat([df_concat, df], ignore_index=True)
 
                         count += 1
-        pocs = [0.2, 0.3, 0.4]
+        fractions_fit = [0.3, 0.4]
         print(df_concat)
         df_concat['Accuracy (%)'] = df_concat['Accuracy'] * 100
         df_concat['Round (t)'] = df_concat['Round']
         # plots
-        self.joint_plot_acc_two_plots(df=df_concat, experiment=1, pocs=pocs)
         # self.joint_plot_acc_two_plots(df=df_concat, experiment=1, pocs=pocs)
-        # self.joint_plot_acc_four_plots(df=df_concat, experiment=1, pocs=pocs)
+        # self.joint_plot_acc_two_plots(df=df_concat, experiment=1, pocs=pocs)
+        self.joint_plot_acc_four_plots(df=df_concat, experiment=1, fractions_fit=fractions_fit)
         # self.joint_plot_acc_two_plots(df=df_concat, experiment=2, pocs=pocs)
-        #self.joint_plot_acc_four_plots(df=df_concat, experiment=2, pocs=pocs)
+        self.joint_plot_acc_four_plots(df=df_concat, experiment=2, fractions_fit=fractions_fit)
         # self.joint_plot_acc_two_plots(df=df_concat, experiment=3, pocs=pocs)
-        #self.joint_plot_acc_four_plots(df=df_concat, experiment=3, pocs=pocs)
+        self.joint_plot_acc_four_plots(df=df_concat, experiment=3, fractions_fit=fractions_fit)
         # self.joint_plot_acc_two_plots(df=df_concat, experiment=4, pocs=pocs)
-        #self.joint_plot_acc_four_plots(df=df_concat, experiment=4, pocs=pocs)
+        self.joint_plot_acc_four_plots(df=df_concat, experiment=4, fractions_fit=fractions_fit)
 
         # table
         # self.joint_table(df_concat, pocs, strategies, experiment=1)
@@ -81,16 +81,16 @@ class JointAnalysis():
     def joint_table(self, df, pocs, strategies, experiment):
 
         model_report = {i: {} for i in strategies}
-        df_test = df[['Round (t)', 'Size of parameters', 'Strategy', 'Accuracy (%)', 'Experiment', 'POC', 'Dataset']].groupby(
-            ['Round (t)', 'Strategy', 'Experiment', 'POC', 'Dataset']).apply(
+        df_test = df[['Round (t)', 'Size of parameters', 'Strategy', 'Accuracy (%)', 'Experiment', 'Fraction fit', 'Dataset']].groupby(
+            ['Round (t)', 'Strategy', 'Experiment', 'Fraction fit', 'Dataset']).apply(
             lambda e: self.groupb_by_table(e)).reset_index()[
-            ['Round (t)', 'Strategy', 'Experiment', 'POC', 'Dataset', 'Size of parameters (bytes)', 'Accuracy (%)']]
+            ['Round (t)', 'Strategy', 'Experiment', 'Fraction fit', 'Dataset', 'Size of parameters (bytes)', 'Accuracy (%)']]
 
         # df_test = df_test.query("""Round in [10, 100]""")
         print("agropou")
         print(df_test)
         convert_dict = {0.1: 5, 0.2: 10, 0.3: 15, 0.4: 20}
-        df_test['POC'] = np.array([convert_dict[i] for i in df_test['POC'].tolist()])
+        df_test['Fraction fit'] = np.array([convert_dict[i] for i in df_test['Fraction fit'].tolist()])
 
         columns = ['15', '20']
 
@@ -155,31 +155,31 @@ class JointAnalysis():
 
         return pd.DataFrame({'Size of parameters (bytes)': [parameters], 'Accuracy (%)': [accuracy], 'Loss': [loss]})
 
-    def filter(self, df, experiment, dataset, poc, strategy=None):
+    def filter(self, df, experiment, dataset, fraction_fit, strategy=None):
 
         # df['Accuracy (%)'] = df['Accuracy (%)']*100
         if strategy is not None:
             df = df.query(
-                """Experiment=={} and POC=={} and Dataset=='{}' and Strategy=='{}'""".format(str(experiment), float(poc), str(dataset), strategy))
+                """Experiment=={} and Fraction fit=={} and Dataset=='{}' and Strategy=='{}'""".format(str(experiment), float(fraction_fit), str(dataset), strategy))
         else:
             df = df.query(
-                """Experiment=={} and POC=={} and Dataset=='{}'""".format(str(experiment), str(poc),
-                                                                                             str(dataset)))
+                """Experiment=={} and Fraction fit=={} and Dataset=='{}'""".format(str(experiment), str(fraction_fit),
+                                                                                   str(dataset)))
         print("filtrou: ", df)
 
         return df
 
-    def filter_and_plot(self, ax, base_dir, filename, title, df, experiment, dataset, poc, x_column, y_column, hue, hue_order=None):
+    def filter_and_plot(self, ax, base_dir, filename, title, df, experiment, dataset, fraction_fit, x_column, y_column, hue, hue_order=None):
 
-        df = self.filter(df, experiment, dataset, poc)
+        df = self.filter(df, experiment, dataset, fraction_fit)
 
         print("filtrado: ", df, df[hue].unique().tolist())
         line_plot(df=df, base_dir=base_dir, file_name=filename, x_column=x_column, y_column=y_column, title=title, hue=hue, ax=ax, type='1', hue_order=hue_order)
 
-    def joint_plot_acc_four_plots(self, df, experiment, pocs):
+    def joint_plot_acc_four_plots(self, df, experiment, fractions_fit):
         print("Joint plot exeprimento: ", experiment)
 
-        df_test = df[['Round (t)', 'Loss', 'Size of parameters', 'Strategy', 'Accuracy (%)', 'Experiment', 'POC', 'Dataset']].groupby(['Round (t)', 'Strategy', 'Experiment', 'POC', 'Dataset']).apply(lambda e: self.groupb_by_plot(e)).reset_index()[['Round (t)', 'Strategy', 'Experiment', 'POC', 'Dataset', 'Size of parameters (bytes)', 'Accuracy (%)', 'Loss']]
+        df_test = df[['Round (t)', 'Loss', 'Size of parameters', 'Strategy', 'Accuracy (%)', 'Experiment', 'Fraction fit', 'Dataset']].groupby(['Round (t)', 'Strategy', 'Experiment', 'POC', 'Dataset']).apply(lambda e: self.groupb_by_plot(e)).reset_index()[['Round (t)', 'Strategy', 'Experiment', 'POC', 'Dataset', 'Size of parameters (bytes)', 'Accuracy (%)', 'Loss']]
         print("agrupou")
         print(df_test)
         # figsize=(12, 9),
@@ -192,15 +192,15 @@ class JointAnalysis():
         plt.ylabel(y_column)
         base_dir = """analysis/output/experiment_{}/""".format(str(experiment+1))
         # ====================================================================
-        poc = pocs[1]
+        fraction_fit = fractions_fit[1]
         dataset = 'MNIST'
-        title = """{} (C={})""".format(dataset, int(float(poc)*50))
+        title = """{} (C={})""".format(dataset, int(float(fraction_fit)*50))
         filename = ''
         i = 0
         j = 0
         hue_order = ['FedPredict', 'FedClassAvg', 'FedPer', 'FedProto', 'FedAvg']
         self.filter_and_plot(ax=axs[i,j], base_dir=base_dir, filename=filename, title=title, df=df_test,
-                             experiment=experiment, dataset=dataset, poc=poc, x_column=x_column, y_column=y_column,
+                             experiment=experiment, dataset=dataset, fraction_fit=fraction_fit, x_column=x_column, y_column=y_column,
                              hue='Strategy', hue_order=hue_order)
         axs[i,j].get_legend().remove()
         axs[i,j].set_xlabel('')
@@ -217,37 +217,37 @@ class JointAnalysis():
         # # add legend to plot
         # plt.legend([handles[idx] for idx in order], [labels[idx] for idx in order])
         # ====================================================================
-        poc = pocs[2]
+        fraction_fit = fractions_fit[2]
         dataset = 'MNIST'
-        title = """{} (C={})""".format(dataset, int(float(poc)*50))
+        title = """{} (C={})""".format(dataset, int(float(fraction_fit)*50))
         i = 0
         j = 1
         self.filter_and_plot(ax=axs[i,j], base_dir=base_dir, filename=filename, title=title, df=df_test,
-                             experiment=experiment, dataset=dataset, poc=poc, x_column=x_column, y_column=y_column,
+                             experiment=experiment, dataset=dataset, fraction_fit=fraction_fit, x_column=x_column, y_column=y_column,
                              hue='Strategy', hue_order=hue_order)
         axs[i,j].get_legend().remove()
         # axs[i].set_xlabel('')
         # axs[i].set_ylabel('')
         # ====================================================================
-        poc = pocs[1]
+        fraction_fit = fractions_fit[1]
         dataset = 'CIFAR10'
-        title = """CIFAR-10 (C={})""".format(int(float(poc)*50))
+        title = """CIFAR-10 (C={})""".format(int(float(fraction_fit)*50))
         i = 1
         j = 0
         self.filter_and_plot(ax=axs[i, j], base_dir=base_dir, filename=filename, title=title, df=df_test,
-                             experiment=experiment, dataset=dataset, poc=poc, x_column=x_column, y_column=y_column,
+                             experiment=experiment, dataset=dataset, fraction_fit=fraction_fit, x_column=x_column, y_column=y_column,
                              hue='Strategy', hue_order=hue_order)
         axs[i, j].get_legend().remove()
         axs[i, j].set_xlabel('')
         axs[i, j].set_ylabel('')
         # ====================================================================
-        poc = pocs[2]
+        fraction_fit = fractions_fit[2]
         dataset = 'CIFAR10'
-        title = """CIFAR-10 (C={})""".format(int(float(poc)*50))
+        title = """CIFAR-10 (C={})""".format(int(float(fraction_fit)*50))
         i = 1
         j = 1
         self.filter_and_plot(ax=axs[i, j], base_dir=base_dir, filename=filename, title=title, df=df_test,
-                             experiment=experiment, dataset=dataset, poc=poc, x_column=x_column, y_column=y_column,
+                             experiment=experiment, dataset=dataset, fraction_fit=fraction_fit, x_column=x_column, y_column=y_column,
                              hue='Strategy', hue_order=hue_order)
         axs[i, j].get_legend().remove()
         axs[i, j].set_xlabel('')
@@ -359,7 +359,7 @@ if __name__ == '__main__':
 
     strategies = ['FedPredict', 'FedAVG', 'FedClassAvg', 'FedPer', 'FedProto']
     # pocs = [0.1, 0.2, 0.3]
-    pocs = [0.2, 0.3, 0.4]
+    fractions_fit = [0.3, 0.4]
     # datasets = ['MNIST', 'CIFAR10']
     datasets = ['MNIST', 'CIFAR10']
     clients = '50'
@@ -368,4 +368,4 @@ if __name__ == '__main__':
     file_type = 'evaluate_client.csv'
 
     joint_plot = JointAnalysis()
-    joint_plot.build_filenames_and_read(type, strategies, pocs, datasets, experiments, clients, model, file_type)
+    joint_plot.build_filenames_and_read(type, strategies, fractions_fit, datasets, experiments, clients, model, file_type)
