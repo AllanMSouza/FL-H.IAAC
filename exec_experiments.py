eee8b7dfc415df219bc1311ec8716c644b7a3838
Cláudio <claudiogs.capanema@gmail.com>
2023-04-24 12:04:03 -0300

    Experimentos

--- exec_experiments.py
+++ exec_experiments.py
@@ -49,17 +49,17 @@ MODELS        					= ['CNN']
 ALGORITHMS    					= ['None', 'POC', 'FedLTA']
 EPOCHS        					= {'1': [1], '2': [3], '3': [1], '4': [1], '5': [2], '6': [1]}
 # CLIENTS       				= {'MNIST': 50, 'CIFAR10': 50, 'CIFAR100': 50, 'MotionSense': 50, 'UCIHAR': 50}
-CLIENTS       					= {'MNIST': [25], 'CIFAR10': [50], 'CIFAR100': [50], 'MotionSense': [24], 'UCIHAR': [30]}
+CLIENTS       					= {'MNIST': [50], 'CIFAR10': [50], 'CIFAR100': [50], 'MotionSense': [24], 'UCIHAR': [30]}
 FRACTION_FIT 					= {'None': [0.4], 'POC': [0], 'FedLTA': [0]}
 POC 							= {'None': [0], 'POC': [0.2], 'FedLTA': [0]}
 DECAY							= {'None': 0, 'POC': 0, 'FedLTA': 0.1}
 NEW_CLIENTS 					= {'None': ['FALSE'], 'POC': ['FALSE', 'TRUE']}
 NEW_CLIENTS_TRAIN 				= {'FALSE': ['FALSE'], 'TRUE': ['FALSE', 'TRUE']}
 # DECAY         				= (0.001, 0.005, 0.009)
-ROUNDS        					= 15
+ROUNDS        					= 100
 # STRATEGIES 					= ('FedPredict', 'FedPer', 'FedClassAvg', 'FedAVG', 'FedClassAvg_with_FedPredict', 'FedPer_with_FedPredict', 'FedProto', 'FedYogi', 'FedLocal',)
-STRATEGIES_FOR_ANALYSIS 		= ['FedProx', 'FedAVG', 'FedPredict']
-STRATEGIES_TO_EXECUTE 			= ['FedProx']
+STRATEGIES_FOR_ANALYSIS 		= ['FedPredict']
+STRATEGIES_TO_EXECUTE 			= ['FedPredict']
 
 EXPERIMENTS 		= {1: {'algorithm': 'None', 'new_client': 'False', 'new_client_train': 'False', 'comment': ''},
 					   2: {'algorithm': 'None', 'new_client': 'False', 'new_client_train': 'False', 'comment': ''},
