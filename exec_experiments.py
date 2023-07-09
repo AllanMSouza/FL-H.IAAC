207bd92a5e73363631877f91fbb78de1f208f7d7
Cláudio <claudiogs.capanema@gmail.com>
2023-07-08 22:38:03 -0300

    Adição da decomposição TSVD

--- exec_experiments.py
+++ exec_experiments.py
@@ -44,9 +44,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 # Configurations
 TYPE = 'torch'
 # DATASETS      				= ['MNIST', 'CIFAR10', 'Tiny-ImageNet']
-DATASETS = ['MNIST']
+DATASETS = ['CIFAR10']
 # DATASETS      					= ['UCIHAR', 'MotionSense']
-MODELS = ['DNN']
+MODELS = ['CNN']
 ALGORITHMS = ['None', 'POC', 'FedLTA']
 EPOCHS = {'1': [1], '2': [1], '3': [1], '4': [1], '5': [2], '6': [1], '7': [1], '8': [1], '9': [1], '10': [1], '11': [1], '12': [1], '13': [1], '14': [1], '15': [1], '16': [1], '17': [1], '18': [1], '19': [1], '20': [1], '21': [1], '22': [1], '23': [1], '24': [1], '25': [1], '26': [1], '27': [1], '28': [1], '29': [1], '30': [1], '31': [1], '32': [1]}
 # CLIENTS       				= {'MNIST': 50, 'CIFAR10': 50, 'CIFAR100': 50, 'MotionSense': 50, 'UCIHAR': 50}
