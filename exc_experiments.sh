095743bd87d480f8175990866b429e3ab00aa90e
Cláudio <claudiogs.capanema@gmail.com>
2023-07-11 16:12:56 -0300

    FedPredict

--- exc_experiments.sh
+++ exc_experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#git checkout FedPer2
+#git checkout FedPredict-v2.1
 #git fetch
 #git pull
 
--- push_logs.sh
+++ push_logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git checkout FedPredict-v2
+git checkout FedPredict-v2.1
 git fetch
 git pull
 
@@ -13,5 +13,5 @@ else
   git config user.email claudiogs.capanema@gmail.com
   git add ./logs
   git commit -m "Resultado de experimento"
-  git push origin FedPredict-v2
+  git push origin FedPredict-v2.1
 fi
